44f08d4b9fad2faedfc200992a61a5fd3dfb6af7
lukaszfranczak <lukaszfranczak@gmail.com>
2018-05-22 10:39:08 +0200

    Parallax - finally working
    Still - the problem with the rectangles (and the solution) is a bit unclear to me

--- 4_Zadania_dodatkowe/8.Parallax/js/app.js
+++ 4_Zadania_dodatkowe/8.Parallax/js/app.js
@@ -1,46 +1,38 @@
 $(document).ready(function () {
-
     var oldMousePositionX = 0;
     var oldMousePositionY = 0;
-
     var currentMousePositionX = 0;
     var currentMousePositionY = 0;
-
     var mouseMoveX = 0;
     var mouseMoveY = 0;
-
     $('.element')
         .each(function() {
-            $(this).css('z-index', $(this).data('z'))
-        })
-        .each(function() {
-            $(this).css('left', $(this).data('x'))
-        })
-        .each(function() {
-            $(this).css('top', $(this).data('y'))
+            $(this).css({
+                'z-index': $(this).data('z'),
+                'left': $(this).data('x'),
+                'top': $(this).data('y')
+            })
         });
-
     $('.scene').on('mouseenter', function (event) {
         oldMousePositionX = event.offsetX;
         oldMousePositionY = event.offsetY;
-    });
 
+    });
     $('.scene').on('mousemove', function (event) {
         currentMousePositionX = event.offsetX;
         currentMousePositionY = event.offsetY;
-
+        if ($(event.target).is($('.element'))) {
+            currentMousePositionX += $(event.target).attr('left');
+            currentMousePositionY += $(event.target).attr('top');
+        }
         mouseMoveX = currentMousePositionX - oldMousePositionX;
         mouseMoveY = currentMousePositionY - oldMousePositionY;
-
         $('.element')
             .each(function() {
-                $(this).css('left', $(this).data('x')+mouseMoveX*$(this).data('speed'))
-            })
-            .each(function() {
-                $(this).css('top', $(this).data('y')+mouseMoveY*$(this).data('speed'))
+                $(this).css({
+                    'left': $(this).data('x')+mouseMoveX*$(this).data('speed'),
+                    'top': $(this).data('y')+mouseMoveY*$(this).data('speed')
+                })
             });
     });
-
-    // Punkt 6 - ??
-
 });
